9a93dd37c6bc1dc47ad0436ecbeab1ff7f2da1d7
CarlosEX <carlos22martinssilva@gmail.com>
2022-11-15 16:13:40 -0500

    conclue estudo de aula sobre metodos e variaveis estaticas

--- metodos_estaticos.py
+++ metodos_estaticos.py
@@ -1,6 +1,12 @@
-class MinhaClasse:
+class Error:
+            
+    @staticmethod
+    def error_500():
+        print('Internal Server Error')
     
-    def __init__(self, estado) -> None:
-        self.estado = estado
+    @staticmethod
+    def error_404():
+        print('Not Found')
         
-    
+Error.error_500()
+Error.error_404()
