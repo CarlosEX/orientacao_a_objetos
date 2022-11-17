7936b32d37d260a90991662daa3aa197bddca0b9
CarlosEX <carlos22martinssilva@gmail.com>
2022-11-17 14:22:06 -0500

    adiciona conclusao da aula de compisicao de classes e objetos

---
+++ programador_lhama/composicao_de_classes/sql_actions/insert.py
@@ -0,0 +1,7 @@
+class Insert:
+
+    def insert_many(self):
+        self('Insert Many')
+
+    def insert_one(self):
+        print('Insert One')
---
+++ programador_lhama/composicao_de_classes/sql_actions/select.py
@@ -0,0 +1,8 @@
+class Select:
+
+    def select_many(self):
+        self('Select Many')
+
+    def select_one(self):
+        print('Select One')
+
