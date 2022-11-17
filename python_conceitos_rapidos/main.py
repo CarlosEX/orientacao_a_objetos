66eafb540c2d6b3343811f31c49ce9a76acc9ccb
CarlosEX <carlos22martinssilva@gmail.com>
2022-11-17 16:03:37 -0500

    adiciona conclusao aula sobre docstring

---
+++ python_conceitos_rapidos/main.py
@@ -0,0 +1,23 @@
+""" Exemplo de Docstring 
+    TypeAnotation:
+
+"""
+
+from typing import Tuple, Dict, Type, List, Union
+
+def agragation(element_1, element_2):
+    """Summary or Description of the Function
+    
+        Documentation reference:
+        https://www.datacamp.com/tutorial/docstrings-python
+
+        Parameters:
+        argument1 (int): Description of arg1
+
+        Returns:
+        int:Returning value
+
+    """
+    return element_1 + element_2
+
+    
